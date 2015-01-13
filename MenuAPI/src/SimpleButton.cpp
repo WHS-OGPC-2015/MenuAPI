593272337794d7f795a051a0b7fe159edf1338b3
NathanJewell <ncjewell@gmail.com>
2015-01-12 21:26:51 -0800

    started work on simple button

--- MenuAPI/src/SimpleButton.cpp
+++ MenuAPI/src/SimpleButton.cpp
@@ -1,1 +1,17 @@
+#include "SimpleButton.h
 
+ SimpelButton::SimpleButton(ofVec2f loc, ofTexture& norm, ofTexture& hov, ofTrueTypeFont* f);
+{
+    location = loc;
+    normal = norm;
+    hover = hov;
+    font = f;
+}
+
+void update(ofVec2f& mousePos)
+{
+    if(pointisin)
+    {
+
+    }
+}
--- MenuAPI/src/SimpleButton.h
+++ MenuAPI/src/SimpleButton.h
@@ -1,10 +1,17 @@
+#pragma once
 #include "MenuEntity.h"
 #include "customMaths.h"
 class SimpleButton : public MenuEntity //child of MenuEntity class
 {
 public:
-    void update();
+    SimpleButton(ofVec2f loc, ofTexture* norm, ofTexture* hov, ofTrueTypeFont* f);//initialize button with position and textures
+
+    void setButtonText();
+    void update(ofVec2f& mousePos);
     void draw();
 private:
-
+    ofTexture* normal; //pointer to default texture
+    ofTexture* hover; //pointer to texture used on mouse hover
+    ofTrueTypeFont* font; //pointer to font for button text
+    std::string text;//text to draw
 };
