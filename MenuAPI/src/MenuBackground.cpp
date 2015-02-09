29300746778316a2dff3fb9a2f63f68e72d3a080
NathanJewell <you@example.com>
2015-02-09 10:05:38 -0800

    initial MenuBackground commit

---
+++ MenuAPI/src/MenuBackground.cpp
@@ -0,0 +1,32 @@
+#include "MenuBackground.h"
+
+
+MenuBackground::MenuBackground(ofVec2f loc, ofTexture& textu)
+{
+    position = loc;
+    backTexture = &textu;
+}
+
+void MenuBackground::update()//MenuBackground doesn't update
+{
+
+}
+void MenuBackground::void update(ofVec2f mousePos, bool clicked);//MenuBackground doesn't update
+{
+
+}
+void MenuBackground::draw()
+{
+    // draws at the top left corner
+    backTexture->draw(ofVec2f(position.x - (backTexture->getWidth())/2, position.y - (backTexture->getHeight())/2));
+}
+bool MenuBackground::getEventDataBool()//no use
+{
+
+}
+int MenuBackground::getEventDataInt()//not going to use this function, Dear Nathan
+{
+
+}
+
+
---
+++ MenuAPI/src/MenuBackground.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "MenuEntity.h"
+
+class MenuBackground : public MenuEntity
+{
+	MenuBackground(ofVec2f loc, ofTexture& textu);//constrctor
+
+	void update();
+	void update(ofVec2f mousePos, bool clicked);
+
+	void draw();
+
+	bool getEventDataBool();
+	int getEventDataInt();
+
+private:
+	ofTexture *backTexture;
+	ofVec2f position;
+
+}
